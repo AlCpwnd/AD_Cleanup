9c7b6407db067223ad3968535fe578b8daddde1a
AlCpwnd <a.dejaeger@hotmail.com>
2022-09-13 15:25:45 +0200

    Added User sorting base and following features:
    - Exceptions: for default / sync accounts
    - Report flag: functionality not yet implemented
    - Server & workstation sorting

--- AD_Cleanup.ps1
+++ AD_Cleanup.ps1
@@ -1,7 +1,9 @@
 param(
-    [Switch]$User=$false,
-    [Switch]$Computer=$false,
-    [Switch]$Server=$false
+    [Switch]$User,
+    [Switch]$Computer,
+    [Switch]$Server,
+    [Switch]$IncludeExceptions,
+    [Switch]$Report
 )
 
 #Requires -Modules ActiveDirectory
@@ -13,10 +15,29 @@ $ComputerGracePeriod = 6 # Number of months of inactivity before workstation/lap
 
 $Today = Get-Date
 
+$UserExceptions = "Guest","DefaultAccount","krbtgt","Sync_" # Exceptions to the search. Either default accounts or sync accounts
+
+# Functions:
+function Show-Status{
+    Param(
+        [Parameter(Mandatory,Position=0)][ValidateSet("Info","Warning","Error")][String]$Type,
+        [Parameter(Mandatory,Position=1)][String]$Message
+    )
+    switch($Type){
+        "Info" {$param = @{Object = "`t[i] $Message"}}
+        "Warning" {$param = @{Object = "`t[!] $Message"; ForegroundColor = "Yellow"}}
+        "Error" {$param = @{Object = "`t[x] $Message"; ForegroundColor = "Red"}}
+    }
+    Write-Host @param
+}
+
 # Code:
 if($User){
-    $Users = Get-ADUser -Filter * -Properties LastLogonDate | Where-Object{$_.LastLogonDate -lt $Today.AddMonths(-$UserGracePeriod)}
-    
+    $Users = Get-ADUser -Filter * -Properties LastLogonDate,CanonicalName | Where-Object{$_.LastLogonDate -lt $Today.AddMonths(-$UserGracePeriod) -and $_.SamAccountName -notmatch ($UserExceptions -join "|")}
+    if($Users){
+        Show-Status Info "$($Users.Count) user(s) found:"
+        $Users | Select-Object Name,SamAccountName,LastLogonDate,CanonicalName
+    }
 }
 if($Server -or $Computer){
     $Devices = Get-ADComputer -Filter * -Properties LastLogonDate,OperatingSystem
