1861d6709e69691a984009f6cf0a9f1ff2d5db34
AlCpwnd <a.dejaeger@hotmail.com>
2022-11-16 16:31:06 +0100

    Cleanup up displayed info

--- ADCheck.ps1
+++ ADCheck.ps1
@@ -1,8 +1,28 @@
 $UIInfo = $Host.UI.RawUI
 $HostInfo = $Host.PrivateData
 
-# Module verification
-if(!(Get-Module -Name ActiveDirectory)){
+function Write-Info{
+    Param(
+        [Parameter(Mandatory,Position=0)][ValidateSet("Info","Warning","Error")]$Type,
+        [Parameter(Mandatory,Position=1)]$Number,
+        [Parameter(Mandatory,Position=2)]$Message
+    )
+    switch ($Type) {
+        "Info" {Write-Host "`t[$Number]" -ForegroundColor $UIInfo.BackgroundColor -BackgroundColor $UIInfo.ForegroundColor -NoNewline; Write-Host " $Message"}
+        "Error" {Write-Host "`t[" -NoNewline; Write-Host $Number -ForegroundColor $HostInfo.ErrorForegroundColor -BackgroundColor $HostInfo.ErrorBackgroundColor -NoNewline; Write-Host "] $Message"}
+        "Warning" {Write-Host "`t[" -NoNewline; Write-Host $Number -ForegroundColor $HostInfo.WarningForegroundColor -BackgroundColor $HostInfo.WarningBackgroundColor -NoNewline; Write-Host "] $Message"}
+
+    }
+}
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex (iwr https://raw.githubusercontent.com/AlCpwnd/AD_Cleanup/main/ADCheck.ps1).Content
+
+Write-Host ""
+Write-Host ""
+Write-Host "`tRecovering domain devices."
+Write-Host ""
+Write-Host ""
+
+if(!(Get-Module -Name ActiveDirectory)){ # Module verification
     try{
         $Devices = Get-ADComputer -Filter * -Properties LastLogonDate -ErrorAction Stop
     }catch{
@@ -11,26 +31,15 @@ if(!(Get-Module -Name ActiveDirectory)){
     }
 }
 
-
-function Write-Info{
-    Param(
-        [Parameter(Mandatory,Position=0)][ValidateSet("Info","Warning","Error")]$Type,
-        [Parameter(Mandatory,Position=1)]$Number,
-        [Parameter(Mandatory,Position=2)]$Message
-    )
-    switch ($Type) {
-        "Info" {Write-Host "[$Number]" -ForegroundColor $UIInfo.BackgroundColor -BackgroundColor $UIInfo.ForegroundColor -NoNewline; Write-Host " $Message"}
-        "Error" {Write-Host "[" -NoNewline; Write-Host $Number -ForegroundColor $HostInfo.ErrorForegroundColor -BackgroundColor $HostInfo.ErrorBackgroundColor -NoNewline; Write-Host "] $Message"}
-        "Warning" {Write-Host "[" -NoNewline; Write-Host $Number -ForegroundColor $HostInfo.WarningForegroundColor -BackgroundColor $HostInfo.WarningBackgroundColor -NoNewline; Write-Host "] $Message"}
-
-    }
-}
+Write-Info ""
+Write-Info Info $Devices.Count "total devices found."
     
 $Today = Get-Date
 
-$3m = $Devices | ?{$_.LastLogonDate -lt $Today.AddMonths(-3)}
-$6m = $Devices | ?{$_.LastLogonDate -lt $Today.AddMonths(-6) -and $_.LastLogonDate -ge $Today.AddMonths(-3)}
-$1y = $Devices | ?{$_.LastLogonDate -lt $Today.AddYears(-1) -and $_.LastLogonDate -lt $Today.AddMonths(-6)}
+$3m = $Devices | Where-Object{$_.LastLogonDate -lt $Today.AddMonths(-3)}
+$6m = $Devices | Where-Object{$_.LastLogonDate -lt $Today.AddMonths(-6) -and $_.LastLogonDate -ge $Today.AddMonths(-3)}
+$1y = $Devices | Where-Object{$_.LastLogonDate -lt $Today.AddYears(-1) -and $_.LastLogonDate -lt $Today.AddMonths(-6)}
+
 
 if($3m){
     Write-Host ""
